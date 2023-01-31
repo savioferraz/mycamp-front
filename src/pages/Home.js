c3ca979550a825c532d54dc87caa3c3c9bee5224
savioferraz <savio.ferraz88@gmail.com>
2023-01-31 12:12:13 -0300

    config libs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
+import Header from '../components/header/Header';
 
 export default function Home() {
   return (
     <Wraper>
       <h1>MyCamp</h1>
+      <Header> aqui</Header>
     </Wraper>
   );
 }
---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+export const resolve = {
+  alias: {
+    '@mui/styled-engine': '@mui/styled-engine-sc',
+  },
+};
